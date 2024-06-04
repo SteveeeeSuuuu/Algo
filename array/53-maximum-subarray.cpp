3adad1df2ec261c9d278a904e27db8054ec41262
YCS <st872530917@gmail.com>
2024-06-04 23:06:52 +0800

    leetcode 53 format

--- array/53-maximum-subarray.cpp
+++ array/53-maximum-subarray.cpp
@@ -9,13 +9,13 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
     	int maxsum = INT_MIN, sum = 0;
-    	for(int n:nums){
-            if(sum < 0){
-    			sum = 0;
-    		}
-    		sum += n;
-    		maxsum = max(maxsum, sum);
-    	}
+    	for(int n:nums) {
+            if(sum < 0) {
+                sum = 0;
+    	   }
+    	   sum += n;
+    	   maxsum = max(maxsum, sum);
+        }
     	return maxsum;
     }
 };
